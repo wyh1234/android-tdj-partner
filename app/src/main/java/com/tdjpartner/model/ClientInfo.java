f7f6b75ecab6f9f3a9dff335c4dc91c1301b5eff
Dragon king <>
2021-06-02 09:38:40 +0800

    下单次数取值字段修改

--- app/src/main/java/com/tdjpartner/model/ClientInfo.java
+++ app/src/main/java/com/tdjpartner/model/ClientInfo.java
@@ -2,97 +2,146 @@ package com.tdjpartner.model;
 
 public class ClientInfo {
 
-    /**
-     * customerId : 16543
-     * auth : 0
-     * name : 良品铺子
-     * address : 汇通新长江中心营销中心汇通新长江中心1层
-     * boss : 张梦雪
-     * mobile : 13671221279
-     * partnerName : null
-     * websiteId : 3
-     * regionCollNo : null
-     * regionNo : null
-     * lineCode : null
-     * customerLineCode : null
-     * todayAmount : null
-     * averageAmount : null
-     * monthTimes : null
-     * notOrderDays : 30
-     * notCallDays : null
-     * lat : 30.593068
-     * lon : 114.349047
-     */
+/**
+    {
+        "customerId":2023999,
+            "auth":0,
+            "name":"18257101000",
+            "address":"汇通新长江中心附近",
+            "boss":"18257101000",
+            "mobile":"18257101000",
+            "partnerName":"甜甜圈圈",
+            "followTime":null,
+            "websiteId":3,
+            "regionCollNo":"E107区-94-1",
+            "regionNo":"107",
+            "lineCode":"94",
+            "customerLineCode":"1",
+            "todayAmount":"0.00",
+            "averageAmount":"0.00",
+            "todayTimes":null,
+            "todayAfterSaleTimes":null,
+            "monthTimes":0,
+            "notOrderDays":null,
+            "notCallDays":null,
+            "userType":1,
+            "monthAmount":null,
+            "afterSaleTimes":null,
+            "noCallDay":null,
+            "receiveName":"18257101000",
+            "receiveMobile":"18257101000",
+            "deliveredTimeBegin":"5: 30",
+            "deliveredTimeEnd":"6: 30",
+            "punchDistance":null,
+            "dayOrderTimes":null,
+            "dayRegisterTimes":null,
+            "lat":"30.593554",
+            "lon":"114.337079",
+            "headUrl":null,
+            "lastMonthActiveNum":null,
+            "activeNum":null,
+            "callNum":null,
+            "monthRegisterNum":null,
+            "monthActiveNum":null,
+            "monthCallNum":null,
+            "lastMonthAvgActiveNum":null,
+            "monthAvgActiveNum":"0.0",
+            "firstOrderNum":null,
+            "lastMonthFirstOrderNum":null,
+            "monthFirstOrderNum":null,
+            "monthAverageAmount":null,
+            "monthAfterSaleTimes":0,
+            "thirtyTimesNum":null,
+            "createTime":"2021-05-21 10:40:13",
+            "yesterdayActiveNum":null,
+            "addMonthAmount":null,
+            "monthAfterSaleAmount":null,
+            "afterSaleAmount":null,
+            "categoryNum":null,
+            "categoryAmount":null,
+            "gradeNextName":null,
+            "grade":0,
+            "driverName":"testLT01",
+            "driverTel":"12345678010",
+            "partnerPhone":null,
+            "licenceDrlCheckStatus":0
+    }*/
 
-    private int customerId;
-    private int auth;
+    private Integer customerId;
+    private Integer auth;
     private String name;
     private String address;
     private String boss;
     private String mobile;
-    private Object partnerName;
-    private int websiteId;
+    private String partnerName;
+    private Object followTime;
+    private Integer websiteId;
     private String regionCollNo;
     private String regionNo;
     private String lineCode;
-    private Object customerLineCode;
-    private Object todayAmount;
-    private Object averageAmount;
-    private Object monthTimes;
-    private int monthAfterSaleTimes;
-    private int notOrderDays;
+    private String customerLineCode;
+    private String todayAmount;
+    private String averageAmount;
+    private Object todayTimes;
+    private Object todayAfterSaleTimes;
+    private Integer monthTimes;
+    private Object notOrderDays;
     private Object notCallDays;
+    private Integer userType;
+    private Object monthAmount;
+    private Object afterSaleTimes;
+    private Object noCallDay;
+    private String receiveName;
+    private String receiveMobile;
+    private String deliveredTimeBegin;
+    private String deliveredTimeEnd;
+    private Object punchDistance;
+    private Object dayOrderTimes;
+    private Object dayRegisterTimes;
     private String lat;
     private String lon;
-    private int userType;
-    private int todayTimes;
-    private int todayAfterSaleTimes;
+    private Object headUrl;
+    private Object lastMonthActiveNum;
+    private Object activeNum;
+    private Object callNum;
+    private Object monthRegisterNum;
+    private Object monthActiveNum;
+    private Object monthCallNum;
+    private Object lastMonthAvgActiveNum;
+    private String monthAvgActiveNum;
+    private Object firstOrderNum;
+    private Object lastMonthFirstOrderNum;
+    private Object monthFirstOrderNum;
+    private Object monthAverageAmount;
+    private Integer monthAfterSaleTimes;
+    private Object thirtyTimesNum;
+    private String createTime;
+    private Object yesterdayActiveNum;
+    private Object addMonthAmount;
+    private Object monthAfterSaleAmount;
+    private int afterSaleAmount;
+    private Object categoryNum;
+    private Object categoryAmount;
+    private Object gradeNextName;
+    private Integer grade;
+    private String driverName;
+    private String driverTel;
+    private Object partnerPhone;
+    private Integer licenceDrlCheckStatus;
 
-    public int getTodayTimes() {
-        return todayTimes;
-    }
-
-    public void setTodayTimes(int todayTimes) {
-        this.todayTimes = todayTimes;
-    }
-
-    public int getTodayAfterSaleTimes() {
-        return todayAfterSaleTimes;
-    }
-
-    public void setTodayAfterSaleTimes(int todayAfterSaleTimes) {
-        this.todayAfterSaleTimes = todayAfterSaleTimes;
-    }
-
-    public int getMonthAfterSaleTimes() {
-        return monthAfterSaleTimes;
-    }
-
-    public void setMonthAfterSaleTimes(int monthAfterSaleTimes) {
-        this.monthAfterSaleTimes = monthAfterSaleTimes;
-    }
-
-    public int getUserType() {
-        return userType;
-    }
-
-    public void setUserType(int userType) {
-        this.userType = userType;
-    }
-
-    public int getCustomerId() {
+    public Integer getCustomerId() {
         return customerId;
     }
 
-    public void setCustomerId(int customerId) {
+    public void setCustomerId(Integer customerId) {
         this.customerId = customerId;
     }
 
-    public int getAuth() {
+    public Integer getAuth() {
         return auth;
     }
 
-    public void setAuth(int auth) {
+    public void setAuth(Integer auth) {
         this.auth = auth;
     }
 
@@ -128,19 +177,27 @@ public class ClientInfo {
         this.mobile = mobile;
     }
 
-    public Object getPartnerName() {
+    public String getPartnerName() {
         return partnerName;
     }
 
-    public void setPartnerName(Object partnerName) {
+    public void setPartnerName(String partnerName) {
         this.partnerName = partnerName;
     }
 
-    public int getWebsiteId() {
+    public Object getFollowTime() {
+        return followTime;
+    }
+
+    public void setFollowTime(Object followTime) {
+        this.followTime = followTime;
+    }
+
+    public Integer getWebsiteId() {
         return websiteId;
     }
 
-    public void setWebsiteId(int websiteId) {
+    public void setWebsiteId(Integer websiteId) {
         this.websiteId = websiteId;
     }
 
@@ -168,43 +225,59 @@ public class ClientInfo {
         this.lineCode = lineCode;
     }
 
-    public Object getCustomerLineCode() {
+    public String getCustomerLineCode() {
         return customerLineCode;
     }
 
-    public void setCustomerLineCode(Object customerLineCode) {
+    public void setCustomerLineCode(String customerLineCode) {
         this.customerLineCode = customerLineCode;
     }
 
-    public Object getTodayAmount() {
+    public String getTodayAmount() {
         return todayAmount;
     }
 
-    public void setTodayAmount(Object todayAmount) {
+    public void setTodayAmount(String todayAmount) {
         this.todayAmount = todayAmount;
     }
 
-    public Object getAverageAmount() {
+    public String getAverageAmount() {
         return averageAmount;
     }
 
-    public void setAverageAmount(Object averageAmount) {
+    public void setAverageAmount(String averageAmount) {
         this.averageAmount = averageAmount;
     }
 
-    public Object getMonthTimes() {
+    public Object getTodayTimes() {
+        return todayTimes;
+    }
+
+    public void setTodayTimes(Object todayTimes) {
+        this.todayTimes = todayTimes;
+    }
+
+    public Object getTodayAfterSaleTimes() {
+        return todayAfterSaleTimes;
+    }
+
+    public void setTodayAfterSaleTimes(Object todayAfterSaleTimes) {
+        this.todayAfterSaleTimes = todayAfterSaleTimes;
+    }
+
+    public Integer getMonthTimes() {
         return monthTimes;
     }
 
-    public void setMonthTimes(Object monthTimes) {
+    public void setMonthTimes(Integer monthTimes) {
         this.monthTimes = monthTimes;
     }
 
-    public int getNotOrderDays() {
+    public Object getNotOrderDays() {
         return notOrderDays;
     }
 
-    public void setNotOrderDays(int notOrderDays) {
+    public void setNotOrderDays(Object notOrderDays) {
         this.notOrderDays = notOrderDays;
     }
 
@@ -214,6 +287,94 @@ public class ClientInfo {
 
     public void setNotCallDays(Object notCallDays) {
         this.notCallDays = notCallDays;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+
+    public Object getMonthAmount() {
+        return monthAmount;
+    }
+
+    public void setMonthAmount(Object monthAmount) {
+        this.monthAmount = monthAmount;
+    }
+
+    public Object getAfterSaleTimes() {
+        return afterSaleTimes;
+    }
+
+    public void setAfterSaleTimes(Object afterSaleTimes) {
+        this.afterSaleTimes = afterSaleTimes;
+    }
+
+    public Object getNoCallDay() {
+        return noCallDay;
+    }
+
+    public void setNoCallDay(Object noCallDay) {
+        this.noCallDay = noCallDay;
+    }
+
+    public String getReceiveName() {
+        return receiveName;
+    }
+
+    public void setReceiveName(String receiveName) {
+        this.receiveName = receiveName;
+    }
+
+    public String getReceiveMobile() {
+        return receiveMobile;
+    }
+
+    public void setReceiveMobile(String receiveMobile) {
+        this.receiveMobile = receiveMobile;
+    }
+
+    public String getDeliveredTimeBegin() {
+        return deliveredTimeBegin;
+    }
+
+    public void setDeliveredTimeBegin(String deliveredTimeBegin) {
+        this.deliveredTimeBegin = deliveredTimeBegin;
+    }
+
+    public String getDeliveredTimeEnd() {
+        return deliveredTimeEnd;
+    }
+
+    public void setDeliveredTimeEnd(String deliveredTimeEnd) {
+        this.deliveredTimeEnd = deliveredTimeEnd;
+    }
+
+    public Object getPunchDistance() {
+        return punchDistance;
+    }
+
+    public void setPunchDistance(Object punchDistance) {
+        this.punchDistance = punchDistance;
+    }
+
+    public Object getDayOrderTimes() {
+        return dayOrderTimes;
+    }
+
+    public void setDayOrderTimes(Object dayOrderTimes) {
+        this.dayOrderTimes = dayOrderTimes;
+    }
+
+    public Object getDayRegisterTimes() {
+        return dayRegisterTimes;
+    }
+
+    public void setDayRegisterTimes(Object dayRegisterTimes) {
+        this.dayRegisterTimes = dayRegisterTimes;
     }
 
     public String getLat() {
@@ -230,5 +391,229 @@ public class ClientInfo {
 
     public void setLon(String lon) {
         this.lon = lon;
+    }
+
+    public Object getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(Object headUrl) {
+        this.headUrl = headUrl;
+    }
+
+    public Object getLastMonthActiveNum() {
+        return lastMonthActiveNum;
+    }
+
+    public void setLastMonthActiveNum(Object lastMonthActiveNum) {
+        this.lastMonthActiveNum = lastMonthActiveNum;
+    }
+
+    public Object getActiveNum() {
+        return activeNum;
+    }
+
+    public void setActiveNum(Object activeNum) {
+        this.activeNum = activeNum;
+    }
+
+    public Object getCallNum() {
+        return callNum;
+    }
+
+    public void setCallNum(Object callNum) {
+        this.callNum = callNum;
+    }
+
+    public Object getMonthRegisterNum() {
+        return monthRegisterNum;
+    }
+
+    public void setMonthRegisterNum(Object monthRegisterNum) {
+        this.monthRegisterNum = monthRegisterNum;
+    }
+
+    public Object getMonthActiveNum() {
+        return monthActiveNum;
+    }
+
+    public void setMonthActiveNum(Object monthActiveNum) {
+        this.monthActiveNum = monthActiveNum;
+    }
+
+    public Object getMonthCallNum() {
+        return monthCallNum;
+    }
+
+    public void setMonthCallNum(Object monthCallNum) {
+        this.monthCallNum = monthCallNum;
+    }
+
+    public Object getLastMonthAvgActiveNum() {
+        return lastMonthAvgActiveNum;
+    }
+
+    public void setLastMonthAvgActiveNum(Object lastMonthAvgActiveNum) {
+        this.lastMonthAvgActiveNum = lastMonthAvgActiveNum;
+    }
+
+    public String getMonthAvgActiveNum() {
+        return monthAvgActiveNum;
+    }
+
+    public void setMonthAvgActiveNum(String monthAvgActiveNum) {
+        this.monthAvgActiveNum = monthAvgActiveNum;
+    }
+
+    public Object getFirstOrderNum() {
+        return firstOrderNum;
+    }
+
+    public void setFirstOrderNum(Object firstOrderNum) {
+        this.firstOrderNum = firstOrderNum;
+    }
+
+    public Object getLastMonthFirstOrderNum() {
+        return lastMonthFirstOrderNum;
+    }
+
+    public void setLastMonthFirstOrderNum(Object lastMonthFirstOrderNum) {
+        this.lastMonthFirstOrderNum = lastMonthFirstOrderNum;
+    }
+
+    public Object getMonthFirstOrderNum() {
+        return monthFirstOrderNum;
+    }
+
+    public void setMonthFirstOrderNum(Object monthFirstOrderNum) {
+        this.monthFirstOrderNum = monthFirstOrderNum;
+    }
+
+    public Object getMonthAverageAmount() {
+        return monthAverageAmount;
+    }
+
+    public void setMonthAverageAmount(Object monthAverageAmount) {
+        this.monthAverageAmount = monthAverageAmount;
+    }
+
+    public Integer getMonthAfterSaleTimes() {
+        return monthAfterSaleTimes;
+    }
+
+    public void setMonthAfterSaleTimes(Integer monthAfterSaleTimes) {
+        this.monthAfterSaleTimes = monthAfterSaleTimes;
+    }
+
+    public Object getThirtyTimesNum() {
+        return thirtyTimesNum;
+    }
+
+    public void setThirtyTimesNum(Object thirtyTimesNum) {
+        this.thirtyTimesNum = thirtyTimesNum;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public Object getYesterdayActiveNum() {
+        return yesterdayActiveNum;
+    }
+
+    public void setYesterdayActiveNum(Object yesterdayActiveNum) {
+        this.yesterdayActiveNum = yesterdayActiveNum;
+    }
+
+    public Object getAddMonthAmount() {
+        return addMonthAmount;
+    }
+
+    public void setAddMonthAmount(Object addMonthAmount) {
+        this.addMonthAmount = addMonthAmount;
+    }
+
+    public Object getMonthAfterSaleAmount() {
+        return monthAfterSaleAmount;
+    }
+
+    public void setMonthAfterSaleAmount(Object monthAfterSaleAmount) {
+        this.monthAfterSaleAmount = monthAfterSaleAmount;
+    }
+
+    public int getAfterSaleAmount() {
+        return afterSaleAmount;
+    }
+
+    public void setAfterSaleAmount(int afterSaleAmount) {
+        this.afterSaleAmount = afterSaleAmount;
+    }
+
+    public Object getCategoryNum() {
+        return categoryNum;
+    }
+
+    public void setCategoryNum(Object categoryNum) {
+        this.categoryNum = categoryNum;
+    }
+
+    public Object getCategoryAmount() {
+        return categoryAmount;
+    }
+
+    public void setCategoryAmount(Object categoryAmount) {
+        this.categoryAmount = categoryAmount;
+    }
+
+    public Object getGradeNextName() {
+        return gradeNextName;
+    }
+
+    public void setGradeNextName(Object gradeNextName) {
+        this.gradeNextName = gradeNextName;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getDriverTel() {
+        return driverTel;
+    }
+
+    public void setDriverTel(String driverTel) {
+        this.driverTel = driverTel;
+    }
+
+    public Object getPartnerPhone() {
+        return partnerPhone;
+    }
+
+    public void setPartnerPhone(Object partnerPhone) {
+        this.partnerPhone = partnerPhone;
+    }
+
+    public Integer getLicenceDrlCheckStatus() {
+        return licenceDrlCheckStatus;
+    }
+
+    public void setLicenceDrlCheckStatus(Integer licenceDrlCheckStatus) {
+        this.licenceDrlCheckStatus = licenceDrlCheckStatus;
     }
 }
